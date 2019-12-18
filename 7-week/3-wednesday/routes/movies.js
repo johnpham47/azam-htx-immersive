9380ead5c45ab461602b6049934992cab5d13ffd
johnpham47 <johnpham47@gmail.com>
2019-12-18 15:24:07 -0600

    Added url param pages

--- 7-week/3-wednesday/routes/movies.js
+++ 7-week/3-wednesday/routes/movies.js
@@ -15,9 +15,9 @@ router.post("/add-movie", (req, res) => {
 });
 
 router.post("/delete-movie", (req, res) => {
-  let trash = req.body.trash;
+  let id = req.body.id;
 
-  movies = movies.filter(movie => movie.title != trash);
+  movies = movies.filter(movie => movie.title != id);
   res.redirect("/movies");
 });
 
@@ -26,8 +26,16 @@ router.get("/", (req, res) => {
   res.render("movies", { movies: movies });
 });
 
-router.get("/genre", (req, res) => {
-  res.render("genre", { movies: movies });
+router.get("/:movieID", (req, res) => {
+  let movieDetails = movies.filter(movie => movie.title === req.params.movieID);
+  console.log("movie details");
+  console.log(movieDetails[0]);
+  res.render("movie", movieDetails[0]);
+});
+
+router.get("/genre/:genre", (req, res) => {
+  console.log(movies);
+  res.render("genres", { movies: movies });
 });
 
 module.exports = router;
