1ba7b43c43f7347ef41e640030c27a684d233e13
johnpham47 <johnpham47@gmail.com>
2020-01-15 09:32:06 -0600

    Fixed chat with other users

--- 10-week/1-monday/chatJsMustache/public/app.js
+++ 10-week/1-monday/chatJsMustache/public/app.js
@@ -2,9 +2,15 @@ const message = document.getElementById("m");
 const submitButton = document.getElementById("submitButton");
 const chatForm = document.getElementById("chatForm")
 const socket = io();
+
+socket.on("chat message", msg => {
+    event.preventDefault();
+    console.log(msg);
+    let messages = document.getElementById("messages");
+    messages.insertAdjacentHTML("beforeend", `<li>${msg}</li>`);
+});
 submitButton.addEventListener("click", event => {
     event.preventDefault()
-    socket.emit("chat message", message);
-    document.getElementById('messages').insertAdjacentHTML('beforeend', `<li>${message.value}</li>`)
+    socket.emit("chat message", message.value);
     chatForm.reset();
 });
